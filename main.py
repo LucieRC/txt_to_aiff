34fed99ebe08baf0adeba0c5eb7155e8c6b82e95
LucieRC <lricqpro@outlook.fr>
2025-01-23 18:19:30 +0100

    Main script  and test.txt file

---
+++ main.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def text_to_speech(text_file, audio_file, voice=None):
+    # Open the text file
+    with open(text_file, 'r') as file:
+        text = file.read()
+
+    # Use the 'say' command to convert text to audio
+    if voice == None:
+        subprocess.run(["say", "-o", audio_file, text])
+    else:
+        subprocess.run(["say", "-v", voice, "-o", audio_file, text])
+
+
+if __name__ == "__main__":
+    language = "English"
+    voice = "Siri" # Can also use the "default" voice
+    if language == "English" and voice == "Siri":
+        voice = "Oliver (Enhanced)"
+    if language == "French" and voice == "Siri":
+        voice = "Thomas (Enhanced)"
+    text_file = "inputs/test.txt"
+    audio_file = "outputs/test.aiff"
+    
+    if voice == "default":
+        text_to_speech(text_file, audio_file)
+    else:
+        text_to_speech(text_file, audio_file, voice)
+
