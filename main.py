21c6f9900c41ad464d9dfa24d4984893ccb479d0
LucieRC <lricqpro@outlook.fr>
2025-01-23 18:27:49 +0100

    Last modif for initial commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import subprocess
+import os
 
 def text_to_speech(text_file, audio_file, voice=None):
+    # Create output directory if it doesn't exist
+    os.makedirs(os.path.dirname(audio_file), exist_ok=True)
+
     # Open the text file
     with open(text_file, 'r') as file:
         text = file.read()
